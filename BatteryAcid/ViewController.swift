ce1568c8e006605e4ca887eadb2cf9f12908bbf4
Mark Brumbaugh <brumbaugh.mark@gmail.com>
2024-09-23 14:56:37 -0700

    Added Text,ImageView, UIPicker

--- BatteryAcid/ViewController.swift
+++ BatteryAcid/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let coffeeArray = ["Pods","Store", "Retail Beans"]
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    @IBAction func goButtonPressed(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
