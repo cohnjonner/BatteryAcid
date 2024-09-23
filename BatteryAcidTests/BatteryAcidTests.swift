62ff0434de1763fa10cea968c47b36a4270f3d31
Mark Brumbaugh <brumbaugh.mark@gmail.com>
2024-09-23 13:57:33 -0700

    Initial Commit

---
+++ BatteryAcidTests/BatteryAcidTests.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryAcidTests.swift
+//  BatteryAcidTests
+//
+//  Created by Mark on 9/23/24.
+//
+
+import Testing
+@testable import BatteryAcid
+
+struct BatteryAcidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
